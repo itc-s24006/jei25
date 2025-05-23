8b87760e6627a84e1b3c254618036a5e92766b1c
itc-s24006 <s24006@std.it-college.ac.jp>
2025-05-23 12:08:25 +0900

    .gitignoreの編集と問題演習追加

---
+++ D49/Q21.java
@@ -0,0 +1,13 @@
+package D49;
+
+public class Q21 {
+    public static void main(String[] args) {
+        int n1 = 0100; //8進数
+        int n2 = 0x10; //16進数　10進数で16
+
+        System.out.println(n1+n2);
+    }
+}
+//「80」と表示されるようにコード修正 l6,8は変更しない
+
+

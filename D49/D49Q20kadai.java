32ec772f20a86dd4662df041728d6bb35ac448ea
itc-s24006 <s24006@std.it-college.ac.jp>
2025-05-23 11:35:17 +0900

    D49Q20課題提出

---
+++ D49/D49Q20kadai.java
@@ -0,0 +1,16 @@
+package D49;
+
+public class D49Q20kadai {
+    public static void main(String[] args) {
+        //double型の二次元配列を5行10列で作成
+        double[][] arr = new double[5][10];
+
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[i].length; j++) {
+//                arr[i][j] = 1.0;
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+    }
+}

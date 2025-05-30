3b281d13881ea61165312445bf1acb64ec4ac0cf
itc-s24006 <s24006@std.it-college.ac.jp>
2025-05-30 12:05:31 +0900

    D50Q25課題提出

---
+++ D50/D50Q25kadai.java
@@ -0,0 +1,10 @@
+package D50;
+
+public class D50Q25kadai {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 8; i++){
+            if (i % 2 == 1) continue;
+            System.out.println(i);
+        }
+    }
+}

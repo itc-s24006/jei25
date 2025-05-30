2dc159f76099ab658aeb324f6ec16dd878e9820f
itc-s24006 <s24006@std.it-college.ac.jp>
2025-05-30 12:22:17 +0900

    D50Q27課題追加

---
+++ D50/D50Q27kadai.java
@@ -0,0 +1,21 @@
+package D50;
+
+public class D50Q27kadai {
+    public static void main(String[] args) {
+        int control = 1;
+        String mode;
+
+        switch (control) {
+            case 1:
+                mode = "Warm";
+                break;
+            case 2:
+                mode = "Cool";
+                break;
+            default:
+                mode = "Wind";
+                break;
+        }
+        System.out.println(mode);
+    }
+}

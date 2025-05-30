c7458aff4178ab342c1c21d39947eb2be42ccf0f
itc-s24006 <s24006@std.it-college.ac.jp>
2025-05-30 11:21:24 +0900

    D50Q18課題提出

---
+++ D50/D50Q18kadai.java
@@ -0,0 +1,10 @@
+package D50;
+
+public class D50Q18kadai {
+    public static void main(String[] args) {
+        int num = 0;
+        while (++num < 11) {
+            System.out.println(num);
+        }
+    }
+}

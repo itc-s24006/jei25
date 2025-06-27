addd14cc8baebe07c2526775f56a175ee4db8a2d
itc-s24006 <s24006@std.it-college.ac.jp>
2025-06-27 12:29:04 +0900

    問題演習・課題提出

---
+++ D51/D51Q23kadai.java
@@ -0,0 +1,10 @@
+package D51;
+
+public class D51Q23kadai {
+    public static void main(String[] args) {
+        String[] season = {"春","夏","秋","冬"};
+        for (String s : season) {
+            System.out.print(s + "。");
+        }
+    }
+}

d01d963525a2e4c2fd3ec3b4f1e4e0626946d58c
itc-s24006 <s24006@std.it-college.ac.jp>
2025-06-27 14:17:41 +0900

    課題提出

---
+++ D51/D51Q21kadai.java
@@ -0,0 +1,17 @@
+package D51;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//異なる基数同士を計算するオリジナルプログラムを作りなさい
+public class D51Q21kadai {
+    public static void main(String[] args) {
+        //10進数と2進数の計算
+        int decimal = 21; //10進数
+        int binary = 0b10101;//2進数
+        System.out.println(decimal * binary);
+
+
+
+    }
+}

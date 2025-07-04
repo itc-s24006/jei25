1d34f5b4e0925247495017f801a877d94de99f3f
itc-s24006 <s24006@std.it-college.ac.jp>
2025-07-04 11:26:27 +0900

    課題提出

---
+++ D51/D51kadai.java
@@ -0,0 +1,10 @@
+package D51;
+
+//実行時に与えられたコマンドライン引数の２番目を実数に変換し、切り上げて表示
+public class D51kadai {
+    public static void main(String[] args) {
+        double toDouble = Double.parseDouble(args[1]);
+        Double toCeil = Math.ceil(toDouble);
+        System.out.println(toCeil);
+    }
+}
---
+++ D51/Q28.java
@@ -0,0 +1,10 @@
+package D51;
+
+public class Q28 {
+    public static void main(String[] args) {
+        double[] level = {0.0, 24.0, 57.0, Double.MAX_VALUE};   //使用量の区分
+        double[] unit = {0.0, 2.0, 3.0, 5.0};    //単位数
+        int[] rate = {0, 19, 17, 14};
+
+    }
+}

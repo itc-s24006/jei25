bd608b5b0e8c625e508fab77cf2607bd9db6442f
itc-s24006 <s24006@std.it-college.ac.jp>
2025-07-04 10:53:33 +0900

    課題提出

---
+++ D51/D51Q27kadai.java
@@ -0,0 +1,9 @@
+package D51;
+
+enum TrafficLight {青, 黄色, 赤}
+public class D51Q27kadai {
+    public static void main(String[] args) {
+        TrafficLight select = TrafficLight.青;
+        System.out.println(select);
+    }
+}
